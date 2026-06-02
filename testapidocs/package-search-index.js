38aca0d1c937d8a85ae19b0ce701946e25a4112a
ste-gr <84541644+ste-gr@users.noreply.github.com>
2026-06-02 02:33:04 +0200

    Site checkin for project Maven Taglib Plugin

--- testapidocs/package-search-index.js
+++ testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"io.github.weblegacy.maven.plugin.taglib"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.weblegacy.maven.plugin.taglib"}];updateSearchResults();
